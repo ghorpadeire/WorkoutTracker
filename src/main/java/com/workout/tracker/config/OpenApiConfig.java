f89b5076e8624ed767d47250416e15dc0981271f
Pranav Ghorpade <pranav.ghorpade3108@gmail.com>
2026-02-05 22:38:45 +0000

    Add OpenAPI configuration class

---
+++ src/main/java/com/workout/tracker/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.workout.tracker.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Workout Tracker API")
+                        .version("1.0.0")
+                        .description("RESTful API for tracking workouts, managing exercises, and generating progress reports")
+                        .contact(new Contact()
+                                .name("Workout Tracker Support")
+                                .email("support@workouttracker.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
+    }
+}
